35ee8aa195c642cb7bc5ef92f55122e0722bc688
Juan Zuleta <juanzuleta@localhost.localdomain>
2019-07-16 12:58:04 -0500

    Actualizado-clase2

--- webpack-ejercicio/src/app.js
+++ webpack-ejercicio/src/app.js
@@ -1,3 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap';
 import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/_custom.scss";
+
+import Photos from "./photos.js";
+
+console.log(Photos);
